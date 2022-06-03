3a28bb19f91943f8bcad750d2bc9ce2ff141faf0
Esma <esmazuurbier12@gmail.com>
2022-06-03 14:58:12 +0200

    load font & added rectangle

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <SFML/Graphics.hpp>
+#include <stdexcept>
 #include "headers/ttt.h"
 
 int main()
 {   
+    // Create window
     sf::RenderWindow window(sf::VideoMode(sf::Vector2u(600, 600)), "Tic Tac Toe");
+
+    // Initialize empty board and players
+    vector<vector< string > > b = initialState();
+    string playerH, playerAI;
+    bool chosenPlayer = false;
+
+    // Load font
+    sf::Font font;
+    if (!font.loadFromFile("resources/tuffy.ttf"))
+    {
+        cout << "Font could not be loaded from file" << endl;
+        return 1;
+    }
 
     // Creating the lines to draw a 3 x 3 board
     sf::RectangleShape hline1(sf::Vector2f(600, 5)); // Horizontal
@@ -28,10 +43,23 @@ int main()
         }
 
         window.clear();
-        window.draw(hline1);
-        window.draw(hline2);
-        window.draw(vline1);
-        window.draw(vline2);
+
+        // Let user choose player 
+        if (chosenPlayer == false) {
+            sf::RectangleShape rectangle(sf::Vector2f(400.f, 150.f));
+            rectangle.setFillColor(sf::Color(0,0,0));
+            rectangle.setOutlineThickness(10.f);
+            rectangle.setPosition(sf::Vector2f(100, 200));
+            sf::Text text;
+            window.draw(rectangle);
+        }
+        else {
+            window.draw(hline1);
+            window.draw(hline2);
+            window.draw(vline1);
+            window.draw(vline2);
+        }
+
         window.display();
     }
 
