bcf2e773f27fe8bccb2fd50eee1716f4ac2b65e8
Esma <esmazuurbier12@gmail.com>
2022-07-03 14:37:34 +0200

    some changes

--- src/point.h
+++ src/point.h
@@ -1,12 +1,14 @@
 class Point {
 public:
-    Point() {x = 0; y = 0;}
     int x;
     int y;
+    Point() {x = 0; y = 0;}
+
     Point(int a, int b) {
         x = a;
         y = b;
     } 
+
     void print() {
         printf("(%i, %i)\n", x, y);
     }
